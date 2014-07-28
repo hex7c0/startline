9b86fd098206b23dced184f77b0a1a48b43cb2f6
hex7c0 <carniellifrancesco@gmail.com>
2014-07-28 20:42:04 +0200

    1.3.1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * @module startline
  * @package startline
  * @subpackage main
- * @version 1.3.0
+ * @version 1.3.1
  * @author hex7c0 <hex7c0@gmail.com>
  * @copyright hex7c0 2014
  * @license GPLv3
